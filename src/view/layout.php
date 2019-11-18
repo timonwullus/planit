3430f69be5e54f15bf840a9f661c649b7a662b46
Timon Wullus <timon.wullus@student.howest.be>
2019-11-18 20:57:44 +0100

    poging 2 met php verbinen

---
+++ src/view/layout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Todos - <?php echo $title; ?></title>
+  </head>
+  <body>
+    <main>
+      <?php
+        if(!empty($_SESSION['error'])) {
+          echo '<div class="error box">' . $_SESSION['error'] . '</div>';
+        }
+        if(!empty($_SESSION['info'])) {
+          echo '<div class="info box">' . $_SESSION['info'] . '</div>';
+        }
+      ?>
+      <header><h1>Todos - <?php echo $title; ?></h1></header>
+      <?php echo $content;?>
+    </main>
+    <script src="http://localhost:8080/script.js"></script>
+  </body>
+</html>
