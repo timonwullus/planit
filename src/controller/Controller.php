6ef742e5893a5fa7981d2b0b4c443e84a88aaee8
Timon Wullus <timon.wullus@student.howest.be>
2019-11-18 21:39:57 +0100

    controller aangepast en css hardcoded in layout

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -4,11 +4,9 @@ class Controller {
 
   public $route;
   protected $viewVars = array();
-  // NEW
   protected $env = 'development';
 
   public function filter() {
-    // NEW
     if (basename(dirname(dirname(__FILE__))) != 'src') {
       $this->env = 'production';
     }
@@ -16,11 +14,13 @@ class Controller {
   }
 
   public function render() {
-    // NEW
     // set js variable according to environment (development / production)
     $this->set('js', '<script src="http://localhost:8080/script.js"></script>'); // webpack dev server
+    // NEW : CSS
+    $this->set('css', ''); // webpack dev server: css is injected by the script
     if ($this->env == 'production') {
       $this->set('js', '<script src="script.js"></script>'); // regular script
+      $this->set('css', '<link href="style.css" rel="stylesheet">'); // regular css tag
     }
     $this->createViewVarWithContent();
     $this->renderInLayout();
--- src/view/layout.php
+++ src/view/layout.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Todos - <?php echo $title; ?></title>
-    <link rel="stylesheet" href="style.css">
+    <?php /* NEW */ ?>
+    <?php echo $css;?>
   </head>
   <body>
     <main>
